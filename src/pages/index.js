c4fa6a6c6ce505e14ae8378bd102599063c1c356
GettyOrawo <gettyorawo@gmail.com>
2018-10-03 08:14:02 +0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hello World!</h1>
+    <p>My name is Getty Orawo. Lets get coding!</p>
+  </Layout>
+)
+
+export default IndexPage
